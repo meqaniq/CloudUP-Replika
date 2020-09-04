c5396ef8e2960850d9b7485a460a8a053273e5ae
Glitch (chimp-altyapi-) <none>
2020-09-04 07:17:51 +0000

    🍬🚨 Checkpoint ./commands/prefix.js:6882615/1587 ./events/message.js:6882615/1

--- commands/prefix.js
+++ commands/prefix.js
@@ -5,28 +5,39 @@ exports.run = async (client, message, args) => {
 if(!message.member.hasPermission('MANAGE_GUILD')) return;
 if(!args[0]) {
 
-  const prefixdata = await data.fetch(`codare3.${message.guild.id}`)
+  const prefixdata = await data.fetch(`codare32.${message.guild.id}`)
   if(prefixdata) {
     
   let i = 0; let checky = "";
-  prefixdata.map(async(codare) => checky += `${i++ +1}. ${data.get(`codare3.${message.guild.id}`)[i-1].prefix}\n`)
+  prefixdata.map(async(codare) => checky += `${i++ +1}. ${data.get(`codare32.${message.guild.id}`)[i-1].prefix2.split(' ')[1]}\n`)
   message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setTitle('Prefixes').setFooter(`${i} prefixes`).setDescription(checky))
     
   } else { message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setTitle('Prefixes').setFooter(`2 prefixes`).setDescription('1. !\n2. ?')) }
  
 } else {
   if(args[0] === 'set') {
-  client.commands.forEach(async p => { await data.push(`codare3.${message.guild.id}`, {prefix2: `${args[1]}${p.help.name} ${args[1]}`}) })
+  client.commands.forEach(async p => { await data.push(`codare32.${message.guild.id}`, {prefix2: `${args[1]}${p.help.name} ${args[1]}`}) })
     
-  data.push(`codare3.${message.guild.id}`, {prefix: args[1]})
-  message.channel.send('tamam') } else { 
-  if(args[0] === 'delete') {
-  client.commands.forEach(async p => { await data.delete(`codare3.${message.guild.id}`, {prefix2: `${args[1]}${p.help.name} ${args[1]}`}) })
+  message.channel.send('tamam') } else if(args[0] === 'delete') {
+  if(args[1]) {
     
-  data.delete(`codare3.${message.guild.id}`, {prefix: args[1]})
-  message.channel.send('ok')
+  let komut = args[1]
+  
+  let komutlar = data.fetch(`codare32.${message.guild.id}`);
+  if(komutlar.length === 2) {
+  data.delete(`codare32.${message.guild.id}`)
+  message.channel.send(`Tag "${komut}" successfully deleted.`)
+  } else {
+  let ex = [];
+  komutlar.forEach(db => {
+  if(db.prefix2.split(' ')[1] === komut) return;
+  ex.push(db)
+  data.set(`codare32.${message.guild.id}`, ex)
+  })
   }
-  }
+  message.channel.send(`Tag "${komut}" successfully deleted.`)
+  } 
+  } 
 }
 };
 exports.conf = {
