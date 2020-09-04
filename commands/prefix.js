a2b1ed5741d6a7b5f2113ca48ae77a1904bd94b7
Glitch (chimp-altyapi-) <none>
2020-09-04 06:19:44 +0000

    🎭🍓 Checkpoint ./events/message.js:6882615/3224 ./commands/prefix.js:6882615/310

--- commands/prefix.js
+++ commands/prefix.js
@@ -8,15 +8,21 @@ if(!args[0]) {
   const prefixdata = await data.fetch(`codare.${message.guild.id}`)
   if(prefixdata) {
     
-  let i = 2; let checky = "";
-  prefixdata.map(async(codare) => checky += `${i++ +1}. ${data.get(`codare.${message.guild.id}`)}`+"\n")
+  let i = 1; let checky = "";
+  prefixdata.map(async(codare) => checky += `${i++ +1}. ${data.get(`codare.${message.guild.id}`)[i-1].prefix}\n`)
   message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setTitle('Prefixes').setFooter(`${i} prefixes`).setDescription(`1. <@!${client.user.id}>\n`+checky))
     
   } else { message.channel.send(new Discord.MessageEmbed().setColor('BLUE').setTitle('Prefixes').setFooter(`3 prefixes`).setDescription(`1. <@!${client.user.id}>\n2. ?\n3. !`)) }
   
 } else {
+  if(args[0] === 'set') {
   data.push(`codare.${message.guild.id}`, {prefix: args[1]})
-  message.channel.send('tamam')
+  message.channel.send('tamam') } else { 
+  if(args[0] === 'delete') {
+  data.delete(`codare.${message.guild.id}`, {prefix: args[1]})
+  message.channel.send('ok')
+  }
+  }
 }
 };
 exports.conf = {
--- events/message.js
+++ events/message.js
@@ -23,8 +23,11 @@ module.exports = async message => {
       message.mentions.members.delete(client.user.id)
       cmd.run(client, message, params, perms);
      }
-} else if(message.content.startsWith('!') || message.content.startsWith('?')) {
-    let command = message.content.split(' ')[0].slice(0);
+} else {
+  let ünlem = '!'
+  let soru = '?'
+  if(message.content.startsWith('!')) {
+    let command = message.content.split(' ')[0].slice(ünlem.length);
     let params = message.content.split(' ').slice(1);
     let perms = client.elevation(message);
     let cmd;
@@ -35,10 +38,25 @@ module.exports = async message => {
     }
     if (cmd) {
       if (perms < cmd.conf.permLevel) return;
-      message.mentions.users.delete(client.user.id)
-      message.mentions.members.delete(client.user.id)
       cmd.run(client, message, params, perms);
      }
+  } else {
+    if(message.content.startsWith('?')) {
+    let command = message.content.split(' ')[0].slice(soru.length);
+    let params = message.content.split(' ').slice(1);
+    let perms = client.elevation(message);
+    let cmd;
+    if (client.commands.has(command)) {
+      cmd = client.commands.get(command);
+    } else if (client.aliases.has(command)) {
+      cmd = client.commands.get(client.aliases.get(command));
+    }
+    if (cmd) {
+      if (perms < cmd.conf.permLevel) return;
+      cmd.run(client, message, params, perms);
+     }
+  }
+  }
 }
     
   } else {
@@ -62,7 +80,7 @@ if(message.content.startsWith(`<@!${client.user.id}>`)) {
 } else {
   let prefixx = prefixdata.find(a => a.prefix === message.content.split(' ')[0].slice(1));
   if(message.content.startsWith(prefixx)) {
-    let command = message.content.split(' ')[0].slice(0);
+    let command = message.content.split(' ')[0].slice(prefixx.length);
     let params = message.content.split(' ').slice(1);
     let perms = client.elevation(message);
     let cmd;
@@ -73,8 +91,6 @@ if(message.content.startsWith(`<@!${client.user.id}>`)) {
     }
     if (cmd) {
       if (perms < cmd.conf.permLevel) return;
-      message.mentions.users.delete(client.user.id)
-      message.mentions.members.delete(client.user.id)
       cmd.run(client, message, params, perms);
      }
 }
